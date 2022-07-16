a5d88b5a7074f64a4c82634ee4ad405b16475b5c
mustafa-ylmaz <98712045+mustafa-ylmaz@users.noreply.github.com>
2022-07-16 15:24:12 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+
+app.use(express.static('public'));
 
 const photo = { 
     id: 1,
     name: 'Photo 1',
     description: 'This is a photo',
 
-}
+} 
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.resolve(__dirname, 'temp', 'index.html'));
 });
 
 
