203bafc6e4c1e36cfd7b7275e2af5c258cdb3307
mustafa-ylmaz <98712045+mustafa-ylmaz@users.noreply.github.com>
2022-07-16 16:11:14 +0300

    partials structure created. template engine ejs added

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const path = require('path');
 
-
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-const photo = { 
-    id: 1,
-    name: 'Photo 1',
-    description: 'This is a photo',
+const photo = {
+  id: 1,
+  name: 'Photo 1',
+  description: 'This is a photo',
 
-} 
+}
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp', 'index.html'));
+  res.render('index')
 });
 
+app.get('/about', (req, res) => {
+  res.render('about')
+});
 
+app.get('/contact', (req, res) => {
+  res.render('contact')
+});
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
-    });
+});
